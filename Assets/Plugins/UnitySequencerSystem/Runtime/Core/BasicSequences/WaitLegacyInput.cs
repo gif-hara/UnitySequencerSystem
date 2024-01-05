faccaef14ca9f58128d003f36b04298fc637566d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-05 21:24:11 +0900

    Add WaitLegacyInput sequence to UnitySequencerSystem

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/BasicSequences/WaitLegacyInput.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace HK.UnitySequencerSystem
+{
+    /// <summary>
+    /// キー入力を待機するシーケンス
+    /// </summary>
+    [Serializable]
+    public sealed class WaitLegacyInput : ISequence
+    {
+        public enum InputKeyType
+        {
+            Down,
+            Up,
+            Press,
+        }
+
+        [SerializeField]
+        private InputKeyType keyPushType;
+
+        [SerializeField]
+        private KeyCode keyCode;
+
+        public WaitLegacyInput(InputKeyType keyPushType, KeyCode keyCode)
+        {
+            this.keyPushType = keyPushType;
+            this.keyCode = keyCode;
+        }
+
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            return UniTask.WaitUntil(() => IsPushed(), cancellationToken: cancellationToken);
+        }
+
+        private bool IsPushed()
+        {
+            switch (this.keyPushType)
+            {
+                case InputKeyType.Down:
+                    return Input.GetKeyDown(this.keyCode);
+                case InputKeyType.Up:
+                    return Input.GetKeyUp(this.keyCode);
+                case InputKeyType.Press:
+                    return Input.GetKey(this.keyCode);
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}

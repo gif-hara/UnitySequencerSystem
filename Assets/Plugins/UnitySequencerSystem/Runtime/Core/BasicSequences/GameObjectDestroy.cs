ebe39ddea1cb6b22edfedd28ab2c2a89f39e7fce
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-06 07:22:15 +0900

    Add GameObjectDestroy sequence implementation

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/BasicSequences/GameObjectDestroy.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace HK.UnitySequencerSystem
+{
+    /// <summary>
+    /// <see cref="GameObject"/>を破棄するシーケンス
+    /// </summary>
+    [Serializable]
+    public sealed class GameObjectDestroy : ISequence
+    {
+        [SerializeField]
+        private string targetName;
+
+        public GameObjectDestroy()
+        {
+        }
+
+        public GameObjectDestroy(string targetName)
+        {
+            this.targetName = targetName;
+        }
+
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var target = container.Resolve<GameObject>(this.targetName);
+            UnityEngine.Object.Destroy(target);
+            return UniTask.CompletedTask;
+        }
+    }
+}

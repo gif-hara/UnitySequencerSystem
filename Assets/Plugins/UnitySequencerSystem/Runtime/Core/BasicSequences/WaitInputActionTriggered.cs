fc6be46dc9c968a902901176f998177eab6c1e79
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-06 00:57:12 +0900

    Add WaitInputActionTriggered sequence to UnitySequencerSystem

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/BasicSequences/WaitInputActionTriggered.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace HK.UnitySequencerSystem
+{
+    /// <summary>
+    /// <see cref="InputAction"/>のトリガーを待機するシーケンス
+    /// </summary>
+    [Serializable]
+    public sealed class WaitInputActionTriggered : ISequence
+    {
+        [SerializeField]
+        private InputActionReference inputActionReference;
+
+        [SerializeField]
+        private PlayerLoopTiming playerLoopTiming = PlayerLoopTiming.Update;
+
+        public WaitInputActionTriggered()
+        {
+        }
+
+        public WaitInputActionTriggered(InputActionReference inputActionReference)
+        {
+            this.inputActionReference = inputActionReference;
+        }
+
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            inputActionReference.action.Enable();
+            return UniTask.WaitUntil(() => IsPushed(), timing: playerLoopTiming, cancellationToken: cancellationToken);
+        }
+
+        private bool IsPushed()
+        {
+            return this.inputActionReference.action.triggered;
+        }
+    }
+}

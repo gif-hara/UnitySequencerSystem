54bc4a7af0d6031694cb825ab9bab109fbea36af
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-05 21:10:53 +0900

    Add PlayAsync method to ISequencer interface

--- Assets/Plugins/UnitySequencerSystem/Runtime/ISequencer.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/ISequencer.cs
@@ -1,6 +1,12 @@
+using Cysharp.Threading.Tasks;
+
 namespace HK.UnitySequencerSystem
 {
     public interface ISequencer
     {
+        /// <summary>
+        /// シーケンスを再生する
+        /// </summary>
+        UniTask PlayAsync(Container container);
     }
 }

fbedcea1caffa0431851d38cdf135c5be5e41c41
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-06 23:26:27 +0900

    Add TransformResolver class to UnitySequencerSystem

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/Resolvers/TransformResolver.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace HK.UnitySequencerSystem.Resolvers
+{
+    public abstract class TransformResolver : IResolver<Transform>
+    {
+        public abstract Transform Resolve(Container container);
+
+        [Serializable]
+        public sealed class ReferenceTransform : TransformResolver
+        {
+            [SerializeField]
+            private Transform target;
+
+            public override Transform Resolve(Container container)
+            {
+                return target;
+            }
+        }
+
+        [Serializable]
+        public sealed class NameTransform : TransformResolver
+        {
+            [SerializeField]
+            private string name;
+
+            public override Transform Resolve(Container container)
+            {
+                return container.Resolve<Transform>(name);
+            }
+        }
+    }
+}

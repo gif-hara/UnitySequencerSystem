bf07a847a372f6a16d736740e33c44e25435e02a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-06 23:06:57 +0900

    Add Vector3Resolver and TransformSetPosition class

--- Assets/Plugins/UnitySequencerSystem/Runtime/Core/BasicSequences/TransformSetPosition.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/BasicSequences/TransformSetPosition.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using HK.UnitySequencerSystem.Resolvers;
 using UnityEngine;
 
 namespace HK.UnitySequencerSystem
@@ -8,14 +9,15 @@ namespace HK.UnitySequencerSystem
     /// <summary>
     /// <see cref="Transform"/>の座標を設定するシーケンス
     /// </summary>
+    [AddTypeMenu("Transform/SetPosition")]
     [Serializable]
     public sealed class TransformSetPosition : ISequence
     {
         [SerializeField]
         private string targetName;
 
-        [SerializeField]
-        private Vector3 position;
+        [SerializeReference, SubclassSelector]
+        private Vector3Resolver positionResolver;
 
         [SerializeField]
         private CoordinateType coordinateType;
@@ -30,23 +32,24 @@ namespace HK.UnitySequencerSystem
         {
         }
 
-        public TransformSetPosition(string targetName, Vector3 position, CoordinateType coordinateType)
+        public TransformSetPosition(string targetName, Vector3Resolver positionResolver, CoordinateType coordinateType)
         {
             this.targetName = targetName;
-            this.position = position;
+            this.positionResolver = positionResolver;
             this.coordinateType = coordinateType;
         }
 
         public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
         {
             var target = container.Resolve<Transform>(this.targetName);
+            var position = this.positionResolver.Resolve(container);
             switch (this.coordinateType)
             {
                 case CoordinateType.World:
-                    target.position = this.position;
+                    target.position = position;
                     break;
                 case CoordinateType.Local:
-                    target.localPosition = this.position;
+                    target.localPosition = position;
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/Resolvers/Vector3Resolver.cs
@@ -0,0 +1,87 @@
+using System;
+using UnityEngine;
+
+namespace HK.UnitySequencerSystem.Resolvers
+{
+    public abstract class Vector3Resolver : IResolver<Vector3>
+    {
+        public abstract Vector3 Resolve(Container container);
+    }
+
+    [Serializable]
+    public sealed class ReferencedTransform : Vector3Resolver
+    {
+        [SerializeField]
+        private Transform target;
+
+        [SerializeField]
+        private CoordinateType coordinateType;
+
+        public enum CoordinateType
+        {
+            World,
+            Local,
+        }
+
+        public override Vector3 Resolve(Container container)
+        {
+            return coordinateType switch
+            {
+                CoordinateType.World => target.position,
+                CoordinateType.Local => target.localPosition,
+                _ => throw new ArgumentOutOfRangeException(),
+            };
+        }
+    }
+
+    [Serializable]
+    public sealed class NamedTransform : Vector3Resolver
+    {
+        [SerializeField]
+        private string name;
+
+        [SerializeField]
+        private CoordinateType coordinateType;
+
+        public enum CoordinateType
+        {
+            World,
+            Local,
+        }
+
+        public override Vector3 Resolve(Container container)
+        {
+            var target = container.Resolve<Transform>(name);
+            return coordinateType switch
+            {
+                CoordinateType.World => target.position,
+                CoordinateType.Local => target.localPosition,
+                _ => throw new ArgumentOutOfRangeException(),
+            };
+        }
+    }
+
+    [Serializable]
+    public sealed class Constant : Vector3Resolver
+    {
+        [SerializeField]
+        private Vector3 value;
+
+        public override Vector3 Resolve(Container container)
+        {
+            return value;
+        }
+    }
+
+    [Serializable]
+    public sealed class NamedVector3 : Vector3Resolver
+    {
+        [SerializeField]
+        private string name;
+
+        public override Vector3 Resolve(Container container)
+        {
+            return container.Resolve<Vector3>(name);
+        }
+    }
+}

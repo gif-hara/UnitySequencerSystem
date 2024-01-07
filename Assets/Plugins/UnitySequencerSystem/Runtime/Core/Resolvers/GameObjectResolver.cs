3d10f33455ce7059cf8dbaa9e584256253faec11
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-07 11:57:52 +0900

    Add GameObjectResolver class to UnitySequencerSystem

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/Resolvers/GameObjectResolver.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace HK.UnitySequencerSystem.Resolvers
+{
+    public abstract class GameObjectResolver : IResolver<GameObject>
+    {
+        public abstract GameObject Resolve(Container container);
+
+        [Serializable]
+        public sealed class ReferenceGameObject : GameObjectResolver
+        {
+            [SerializeField]
+            private GameObject target;
+
+            public override GameObject Resolve(Container container)
+            {
+                return target;
+            }
+        }
+
+        [Serializable]
+        public sealed class NameGameObject : GameObjectResolver
+        {
+            [SerializeField]
+            private string name;
+
+            public override GameObject Resolve(Container container)
+            {
+                return container.Resolve<GameObject>(name);
+            }
+        }
+    }
+}

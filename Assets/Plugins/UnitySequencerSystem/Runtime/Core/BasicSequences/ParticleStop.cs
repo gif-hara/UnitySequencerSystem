5a3df10ac72989ae1c938dfa4099129b32c1c30c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-07 13:36:36 +0900

    Add ParticleStop sequence to UnitySequencerSystem

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/BasicSequences/ParticleStop.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK.UnitySequencerSystem.Resolvers;
+using UnityEngine;
+
+namespace HK.UnitySequencerSystem
+{
+    /// <summary>
+    /// <see cref="ParticleSystem"/>を停止するシーケンス
+    /// </summary>
+    [AddTypeMenu("Particle/Stop")]
+    [Serializable]
+    public sealed class ParticleStop : ISequence
+    {
+        [SerializeReference, SubclassSelector]
+        private ParticleSystemResolver targetResolver;
+
+        [SerializeField]
+        private bool withChildren = true;
+
+        [SerializeField]
+        private ParticleSystemStopBehavior stopBehavior = ParticleSystemStopBehavior.StopEmitting;
+
+        public ParticleStop()
+        {
+        }
+
+        public ParticleStop(ParticleSystemResolver targetResolver, bool withChildren, ParticleSystemStopBehavior stopBehavior)
+        {
+            this.targetResolver = targetResolver;
+            this.withChildren = withChildren;
+            this.stopBehavior = stopBehavior;
+        }
+
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            var target = this.targetResolver.Resolve(container);
+            target.Stop(withChildren, stopBehavior);
+            return UniTask.CompletedTask;
+        }
+    }
+}

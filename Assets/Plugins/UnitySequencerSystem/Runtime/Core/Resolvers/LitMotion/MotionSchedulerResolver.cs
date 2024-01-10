c4bf2ab031e1786bac7bfcf6125d0726ffc77511
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-10 16:22:56 +0900

    Add MotionSchedulerResolver class to UnitySequencerSystem

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/Resolvers/LitMotion/MotionSchedulerResolver.cs
@@ -0,0 +1,46 @@
+using System;
+using LitMotion;
+
+namespace HK.UnitySequencerSystem.Resolvers.LitMotion
+{
+    public abstract class MotionSchedulerResolver : IResolver<IMotionScheduler>
+    {
+        public abstract IMotionScheduler Resolve(Container container);
+
+        [Serializable]
+        public sealed class Update : MotionSchedulerResolver
+        {
+            public override IMotionScheduler Resolve(Container container)
+            {
+                return MotionScheduler.Update;
+            }
+        }
+        
+        [Serializable]
+        public sealed class LateUpdate : MotionSchedulerResolver
+        {
+            public override IMotionScheduler Resolve(Container container)
+            {
+                return MotionScheduler.LateUpdate;
+            }
+        }
+        
+        [Serializable]
+        public sealed class FixedUpdate : MotionSchedulerResolver
+        {
+            public override IMotionScheduler Resolve(Container container)
+            {
+                return MotionScheduler.FixedUpdate;
+            }
+        }
+        
+        [Serializable]
+        public sealed class Manual : MotionSchedulerResolver
+        {
+            public override IMotionScheduler Resolve(Container container)
+            {
+                return MotionScheduler.Manual;
+            }
+        }
+    }
+}

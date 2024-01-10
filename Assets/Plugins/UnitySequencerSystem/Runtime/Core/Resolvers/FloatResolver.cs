6d784acef93413b78d7d2c90820f6a5564e9fe00
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-10 16:03:13 +0900

    Add FloatResolver class to UnitySequencerSystem

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/Resolvers/FloatResolver.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace HK.UnitySequencerSystem.Resolvers
+{
+    public abstract class FloatResolver : IResolver<float>
+    {
+        public abstract float Resolve(Container container);
+
+        [Serializable]
+        public sealed class Constant : FloatResolver
+        {
+            [SerializeField]
+            private float value;
+
+            public override float Resolve(Container container)
+            {
+                return value;
+            }
+        }
+
+        [Serializable]
+        public sealed class NameFloat : FloatResolver
+        {
+            [SerializeField]
+            private string name;
+
+            public override float Resolve(Container container)
+            {
+                return container.Resolve<float>(name);
+            }
+        }
+    }
+}

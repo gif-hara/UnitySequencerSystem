94bf908c073f11de836dc5f652610325250634c8
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-10 14:43:08 +0900

    Add TMP_TextResolver class for resolving TMP_Text objects

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/Resolvers/TMP_TextResolver.cs
@@ -0,0 +1,35 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace HK.UnitySequencerSystem.Resolvers
+{
+    public abstract class TMP_TextResolver : IResolver<TMP_Text>
+    {
+        public abstract TMP_Text Resolve(Container container);
+
+        [Serializable]
+        public sealed class ReferenceTMP_Text : TMP_TextResolver
+        {
+            [SerializeField]
+            private TMP_Text target;
+
+            public override TMP_Text Resolve(Container container)
+            {
+                return target;
+            }
+        }
+
+        [Serializable]
+        public sealed class NameTMP_Text : TMP_TextResolver
+        {
+            [SerializeField]
+            private string name;
+
+            public override TMP_Text Resolve(Container container)
+            {
+                return container.Resolve<TMP_Text>(name);
+            }
+        }
+    }
+}

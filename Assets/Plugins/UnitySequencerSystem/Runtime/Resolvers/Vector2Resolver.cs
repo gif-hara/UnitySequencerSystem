904e85e6e38c49cd6e4dd13326c4ca38c4ecc37a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-12 00:48:16 +0900

    Add Vector2Resolver class to UnitySequencerSystem

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/Vector2Resolver.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+namespace UnitySequencerSystem.Resolvers
+{
+    public abstract class Vector2Resolver : IResolver<Vector2>
+    {
+        public abstract Vector2 Resolve(Container container);
+
+#if USS_SUPPORT_SUB_CLASS_SELECTOR
+        [AddTypeMenu("Constant")]
+#endif
+        [Serializable]
+        public sealed class Constant : Vector2Resolver
+        {
+            [SerializeField]
+            private Vector2 value;
+
+            public override Vector2 Resolve(Container container)
+            {
+                return value;
+            }
+        }
+
+#if USS_SUPPORT_SUB_CLASS_SELECTOR
+        [AddTypeMenu("Name")]
+#endif
+        [Serializable]
+        public sealed class NameVector2 : Vector2Resolver
+        {
+            [SerializeField]
+            private string name;
+
+            public override Vector2 Resolve(Container container)
+            {
+                return container.Resolve<Vector2>(name);
+            }
+        }
+    }
+}

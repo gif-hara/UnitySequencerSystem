1a431fd6599e2540de17b32a45247a9626649b01
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-11 16:34:23 +0900

    Add GameObjectInstantiate sequence to UnitySequencerSystem

--- Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/GameObject.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/GameObject.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using UnityEngine;
+using UnitySequencerSystem.Resolvers;
 #if USS_SUPPORT_UNITASK
 using Cysharp.Threading.Tasks;
 #else
@@ -9,6 +10,53 @@ using System.Threading.Tasks;
 
 namespace UnitySequencerSystem.StandardSequences
 {
+    [AddTypeMenu("Standard/GameObject Instantiate")]
+    [Serializable]
+    public sealed class GameObjectInstantiate : ISequence
+    {
+#if USS_SUPPORT_SUB_CLASS_SELECTOR
+        [SubclassSelector]
+#endif
+        [SerializeReference]
+        private GameObjectResolver prefabResolver;
+
+#if USS_SUPPORT_SUB_CLASS_SELECTOR
+        [SubclassSelector]
+#endif
+        [SerializeReference]
+        private StringResolver containerNameResolver;
+
+        public GameObjectInstantiate()
+        {
+        }
+        
+        public GameObjectInstantiate(GameObjectResolver prefabResolver, StringResolver containerNameResolver)
+        {
+            this.prefabResolver = prefabResolver;
+            this.containerNameResolver = containerNameResolver;
+        }
+        
+#if USS_SUPPORT_UNITASK
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+#else
+        public Task PlayAsync(Container container, CancellationToken cancellationToken)
+#endif
+        {
+            var prefab = this.prefabResolver.Resolve(container);
+            var instance = UnityEngine.Object.Instantiate(prefab);
+            if (containerNameResolver != null)
+            {
+                var containerName = containerNameResolver.Resolve(container);
+                container.RegisterOrReplace(containerName, instance);
+            }
+#if USS_SUPPORT_UNITASK
+            return UniTask.CompletedTask;
+#else
+            return Task.CompletedTask;
+#endif
+        }
+    }
+
     /// <summary>
     /// <see cref="GameObject"/>を破棄するシーケンス
     /// </summary>

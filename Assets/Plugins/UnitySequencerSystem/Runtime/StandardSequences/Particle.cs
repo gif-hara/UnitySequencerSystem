0ce95faa313509175b02db0acc82ad42ea5c429b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-11 14:37:06 +0900

    Refactor particle sequences

--- Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/Particle.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/Particle.cs
@@ -10,91 +10,91 @@ using System.Threading.Tasks;
 
 namespace UnitySequencerSystem.StandardSequences
 {
-        /// <summary>
-        /// <see cref="ParticleSystem"/>を再生するシーケンス
-        /// </summary>
-        [AddTypeMenu("Standard/Particle Play")]
-        [Serializable]
-        public sealed class ParticlePlay : ISequence
-        {
-#if USS_SUPPORT_SUB_CLASS_SELECTOR
-                [SubclassSelector]
-#endif
-                [SerializeReference]
-                private ParticleSystemResolver targetResolver;
-
-                [SerializeField]
-                private bool withChildren = true;
-
-                public ParticlePlay()
-                {
-                }
-
-                public ParticlePlay(ParticleSystemResolver targetResolver, bool withChildren)
-                {
-                        this.targetResolver = targetResolver;
-                        this.withChildren = withChildren;
-                }
-
-#if USS_SUPPORT_UNITASK
-                public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
-#else
-                public Task PlayAsync(Container container, CancellationToken cancellationToken)
-#endif
-                {
-                        var target = this.targetResolver.Resolve(container);
-                        target.Play(withChildren);
-#if USS_SUPPORT_UNITASK
-                        return UniTask.CompletedTask;
-#else
-                        return Task.CompletedTask;
-#endif
-                }
-        }
-        
-        /// <summary>
-        /// <see cref="ParticleSystem"/>を停止するシーケンス
-        /// </summary>
-        [AddTypeMenu("Standard/Particle Stop")]
-        [Serializable]
-        public sealed class ParticleStop : ISequence
-        {
-#if USS_SUPPORT_SUB_CLASS_SELECTOR
+    /// <summary>
+    /// <see cref="ParticleSystem"/>を再生するシーケンス
+    /// </summary>
+    [AddTypeMenu("Standard/Particle Play")]
+    [Serializable]
+    public sealed class ParticlePlay : ISequence
+    {
+        #if USS_SUPPORT_SUB_CLASS_SELECTOR
         [SubclassSelector]
-#endif
-                [SerializeReference]
-                private ParticleSystemResolver targetResolver;
+        #endif
+        [SerializeReference]
+        private ParticleSystemResolver targetResolver;
 
-                [SerializeField]
-                private bool withChildren = true;
+        [SerializeField]
+        private bool withChildren = true;
 
-                [SerializeField]
-                private ParticleSystemStopBehavior stopBehavior = ParticleSystemStopBehavior.StopEmitting;
-
-                public ParticleStop()
-                {
-                }
-
-                public ParticleStop(ParticleSystemResolver targetResolver, bool withChildren, ParticleSystemStopBehavior stopBehavior)
-                {
-                        this.targetResolver = targetResolver;
-                        this.withChildren = withChildren;
-                        this.stopBehavior = stopBehavior;
-                }
-
-#if USS_SUPPORT_UNITASK
-                public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
-#else
-        public Task PlayAsync(Container container, CancellationToken cancellationToken)
-#endif
-                {
-                        var target = this.targetResolver.Resolve(container);
-                        target.Stop(withChildren, stopBehavior);
-#if USS_SUPPORT_UNITASK
-                        return UniTask.CompletedTask;
-#else
-            return Task.CompletedTask;
-#endif
-                }
+        public ParticlePlay()
+        {
         }
+
+        public ParticlePlay(ParticleSystemResolver targetResolver, bool withChildren)
+        {
+            this.targetResolver = targetResolver;
+            this.withChildren = withChildren;
+        }
+
+        #if USS_SUPPORT_UNITASK
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        #else
+        public Task PlayAsync(Container container, CancellationToken cancellationToken)
+        #endif
+        {
+            var target = this.targetResolver.Resolve(container);
+            target.Play(withChildren);
+            #if USS_SUPPORT_UNITASK
+            return UniTask.CompletedTask;
+            #else
+            return Task.CompletedTask;
+            #endif
+        }
+    }
+        
+    /// <summary>
+    /// <see cref="ParticleSystem"/>を停止するシーケンス
+    /// </summary>
+    [AddTypeMenu("Standard/Particle Stop")]
+    [Serializable]
+    public sealed class ParticleStop : ISequence
+    {
+        #if USS_SUPPORT_SUB_CLASS_SELECTOR
+        [SubclassSelector]
+        #endif
+        [SerializeReference]
+        private ParticleSystemResolver targetResolver;
+
+        [SerializeField]
+        private bool withChildren = true;
+
+        [SerializeField]
+        private ParticleSystemStopBehavior stopBehavior = ParticleSystemStopBehavior.StopEmitting;
+
+        public ParticleStop()
+        {
+        }
+
+        public ParticleStop(ParticleSystemResolver targetResolver, bool withChildren, ParticleSystemStopBehavior stopBehavior)
+        {
+            this.targetResolver = targetResolver;
+            this.withChildren = withChildren;
+            this.stopBehavior = stopBehavior;
+        }
+
+        #if USS_SUPPORT_UNITASK
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        #else
+        public Task PlayAsync(Container container, CancellationToken cancellationToken)
+        #endif
+        {
+            var target = this.targetResolver.Resolve(container);
+            target.Stop(withChildren, stopBehavior);
+            #if USS_SUPPORT_UNITASK
+            return UniTask.CompletedTask;
+            #else
+            return Task.CompletedTask;
+            #endif
+        }
+    }
 }

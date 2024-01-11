09f5aa18d992d706da5a8b458382d84d09fe7379
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-11 23:10:10 +0900

    Add SequencesResolver class with different resolver types

---
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/SequencesResolver.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnitySequencerSystem.Resolvers
+{
+    public abstract class SequencesResolver : IResolver<List<ISequence>>
+    {
+        public abstract List<ISequence> Resolve(Container container);
+
+        [AddTypeMenu("List")]
+        [Serializable]
+        public sealed class List : SequencesResolver
+        {
+            [SerializeField]
+            private List<ISequence> value;
+
+            public override List<ISequence> Resolve(Container container)
+            {
+                return value;
+            }
+        }
+
+        [AddTypeMenu("Name")]
+        [Serializable]
+        public sealed class Name : SequencesResolver
+        {
+            [SerializeField]
+            private string name;
+
+            public override List<ISequence> Resolve(Container container)
+            {
+                return container.Resolve<List<ISequence>>(name);
+            }
+        }
+
+        [AddTypeMenu("Func")]
+        [Serializable]
+        public sealed class Func : SequencesResolver
+        {
+            [SerializeField]
+            private string name;
+
+            public override List<ISequence> Resolve(Container container)
+            {
+                return container.Resolve<Func<List<ISequence>>>(name)();
+            }
+        }
+
+        [AddTypeMenu("ScriptableObject")]
+        [Serializable]
+        public sealed class ScriptableObject : SequencesResolver
+        {
+            [SerializeField]
+            private ScriptableSequences value;
+
+            public override List<ISequence> Resolve(Container container)
+            {
+                return value.Sequences;
+            }
+        }
+    }
+}

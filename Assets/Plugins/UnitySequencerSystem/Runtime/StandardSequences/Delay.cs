db8eda6edd0b9be1a59262d008ae7626986e295f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-11 14:41:20 +0900

    Add DelayFrame sequence class

--- Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/Delay.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/Delay.cs
@@ -40,4 +40,44 @@ namespace UnitySequencerSystem.StandardSequences
         }
 #endif
     }
+
+    /// <summary>
+    /// 指定したフレーム待機するシーケンス
+    /// </summary>
+    [AddTypeMenu("Standard/Delay Frame")]
+    [Serializable]
+    public sealed class DelayFrame : ISequence
+    {
+        [SerializeField]
+        private int frames;
+
+        public DelayFrame()
+        {
+        }
+
+        public DelayFrame(int frames)
+        {
+            this.frames = frames;
+        }
+
+#if USS_SUPPORT_UNITASK
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+#else
+        public Task PlayAsync(Container container, CancellationToken cancellationToken)
+#endif
+        {
+#if USS_SUPPORT_UNITASK
+            return UniTask.DelayFrame(this.frames, cancellationToken: cancellationToken);
+#else
+            IEnumerator DelayFrame()
+            {
+                for (var i = 0; i < this.frames; i++)
+                {
+                    yield return null;
+                }
+            }
+            return MainThreadDispatcher.Instance.RunCoroutineAsTask(DelayFrame());
+#endif
+        }
+    }
 }

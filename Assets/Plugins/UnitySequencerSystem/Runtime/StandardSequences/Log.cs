ae91869bc265bda16be605247f58c4063761237e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-12 01:19:44 +0900

    Add LogVector3 sequence class

--- Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/Log.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/Log.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Threading;
 using UnityEngine;
+using UnitySequencerSystem.Resolvers;
+
 #if USS_SUPPORT_UNITASK
 using Cysharp.Threading.Tasks;
 #else
@@ -34,6 +36,46 @@ namespace UnitySequencerSystem.StandardSequences
 #endif
         {
             Debug.Log(this.message);
+#if USS_SUPPORT_UNITASK
+            return UniTask.CompletedTask;
+#else
+            return Task.CompletedTask;
+#endif
+        }
+    }
+
+#if USS_SUPPORT_SUB_CLASS_SELECTOR
+    [AddTypeMenu("Standard/Log Vector3")]
+#endif
+    [Serializable]
+    public sealed class LogVector3 : ISequence
+    {
+        [SerializeField]
+        private string header;
+
+#if USS_SUPPORT_SUB_CLASS_SELECTOR
+        [SubclassSelector]
+#endif
+        [SerializeReference]
+        private Vector3Resolver vector3Resolver;
+
+        public LogVector3()
+        {
+        }
+
+        public LogVector3(string header, Vector3Resolver vector3Resolver)
+        {
+            this.header = header;
+            this.vector3Resolver = vector3Resolver;
+        }
+
+#if USS_SUPPORT_UNITASK
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+#else
+        public Task PlayAsync(Container container, CancellationToken cancellationToken)
+#endif
+        {
+            Debug.Log($"{header}{vector3Resolver.Resolve(container)}");
 #if USS_SUPPORT_UNITASK
             return UniTask.CompletedTask;
 #else

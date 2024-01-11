6154581d61dba243f4a08d29fa497dd0080d4574
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-11 11:24:47 +0900

    Refactor PlayAsync method to support both UniTask and Task

--- Assets/Plugins/UnitySequencerSystem/Runtime/Core/Standard/TMP_TextSetText.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Core/Standard/TMP_TextSetText.cs
@@ -37,12 +37,20 @@ namespace HK.UnitySequencerSystem.Standard
             this.textResolver = textResolver;
         }
 
+#if USS_SUPPORT_UNITASK
         public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+#else
+        public Task PlayAsync(Container container, CancellationToken cancellationToken)
+#endif
         {
             var target = targetResolver.Resolve(container);
             var text = textResolver.Resolve(container);
             target.SetText(text);
+#if USS_SUPPORT_UNITASK
             return UniTask.CompletedTask;
+#else
+            return Task.CompletedTask;
+#endif
         }
     }
 }

242e5b600e40878a3add16ed51ecd8f9d2b8582f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-13 00:02:45 +0900

    Add InputActionReadValue classes for Vector3, bool, and float

--- Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/InputSystem.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/InputSystem.cs
@@ -54,5 +54,119 @@ namespace UnitySequencerSystem.StandardSequences
 #endif
         }
     }
+
+#if USS_SUPPORT_SUB_CLASS_SELECTOR
+    [AddTypeMenu("Standard/Input Action Read Value/Vector3")]
+#endif
+    [Serializable]
+    public sealed class InputActionReadValueVector3 : Sequence
+    {
+        [SerializeField]
+        private InputActionReference inputActionReference;
+
+        [SerializeField]
+        private string vector3Name;
+
+        public InputActionReadValueVector3()
+        {
+        }
+
+        public InputActionReadValueVector3(InputActionReference inputActionReference)
+        {
+            this.inputActionReference = inputActionReference;
+        }
+
+#if USS_SUPPORT_UNITASK
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+#else
+        public override Task PlayAsync(Container container, CancellationToken cancellationToken)
+#endif
+        {
+            inputActionReference.action.Enable();
+            var value = inputActionReference.action.ReadValue<Vector3>();
+            container.RegisterOrReplace(vector3Name, value);
+#if USS_SUPPORT_UNITASK
+            return UniTask.CompletedTask;
+#else
+            return Task.CompletedTask;
+#endif
+        }
+    }
+
+#if USS_SUPPORT_SUB_CLASS_SELECTOR
+    [AddTypeMenu("Standard/Input Action Read Value/bool")]
+#endif
+    [Serializable]
+    public sealed class InputActionReadValueBool : Sequence
+    {
+        [SerializeField]
+        private InputActionReference inputActionReference;
+
+        [SerializeField]
+        private string boolName;
+
+        public InputActionReadValueBool()
+        {
+        }
+
+        public InputActionReadValueBool(InputActionReference inputActionReference)
+        {
+            this.inputActionReference = inputActionReference;
+        }
+
+#if USS_SUPPORT_UNITASK
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+#else
+        public override Task PlayAsync(Container container, CancellationToken cancellationToken)
+#endif
+        {
+            inputActionReference.action.Enable();
+            var value = inputActionReference.action.ReadValue<bool>();
+            container.RegisterOrReplace(boolName, value);
+#if USS_SUPPORT_UNITASK
+            return UniTask.CompletedTask;
+#else
+            return Task.CompletedTask;
+#endif
+        }
+    }
+
+#if USS_SUPPORT_SUB_CLASS_SELECTOR
+    [AddTypeMenu("Standard/Input Action Read Value/float")]
+#endif
+    [Serializable]
+    public sealed class InputActionReadValueFloat : Sequence
+    {
+        [SerializeField]
+        private InputActionReference inputActionReference;
+
+        [SerializeField]
+        private string floatName;
+
+        public InputActionReadValueFloat()
+        {
+        }
+
+        public InputActionReadValueFloat(InputActionReference inputActionReference)
+        {
+            this.inputActionReference = inputActionReference;
+        }
+
+#if USS_SUPPORT_UNITASK
+        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+#else
+        public override Task PlayAsync(Container container, CancellationToken cancellationToken)
+#endif
+        {
+            inputActionReference.action.Enable();
+            var value = inputActionReference.action.ReadValue<float>();
+            container.RegisterOrReplace(floatName, value);
+#if USS_SUPPORT_UNITASK
+            return UniTask.CompletedTask;
+#else
+            return Task.CompletedTask;
+#endif
+        }
+    }
 }
 #endif

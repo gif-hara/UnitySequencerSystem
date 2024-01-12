c1746eaafea547e494f6be533b82174e574b840c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-12 23:59:39 +0900

    Refactor InputActionPerformed class

--- Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/InputSystem.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/InputSystem.cs
@@ -54,49 +54,5 @@ namespace UnitySequencerSystem.StandardSequences
 #endif
         }
     }
-
-#if USS_SUPPORT_SUB_CLASS_SELECTOR
-    [AddTypeMenu("Standard/Input Action Performed")]
-#endif
-    [Serializable]
-    public sealed class InputActionPerformed : Sequence
-    {
-        [SerializeField]
-        private InputActionReference inputActionReference;
-
-        [SerializeField]
-        private SequencesResolver sequencesResolver;
-
-        public InputActionPerformed()
-        {
-        }
-
-        public InputActionPerformed(InputActionReference inputActionReference, SequencesResolver sequencesResolver)
-        {
-            this.inputActionReference = inputActionReference;
-            this.sequencesResolver = sequencesResolver;
-        }
-
-#if USS_SUPPORT_UNITASK
-        public override UniTask PlayAsync(Container container, CancellationToken cancellationToken)
-#else
-        public override Task PlayAsync(Container container, CancellationToken cancellationToken)
-#endif
-        {
-            inputActionReference.action.Enable();
-            inputActionReference.action.performed += OnPerformed;
-            void OnPerformed(InputAction.CallbackContext context)
-            {
-                var sequences = sequencesResolver.Resolve(container);
-                var sequencer = new Sequencer(container, sequences);
-                sequencer.PlayAsync(cancellationToken).Forget();
-            }
-#if USS_SUPPORT_UNITASK
-            return UniTask.CompletedTask;
-#else
-            return Task.CompletedTask;
-#endif
-        }
-    }
 }
 #endif

13c41a7784152f6cb860332021a119778c3b4b90
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-14 22:01:50 +0900

    Remove unnecessary comments from IResolver.cs

--- Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/IResolver.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/IResolver.cs
@@ -1,13 +1,7 @@
 namespace UnitySequencerSystem.Resolvers
 {
-    /// <summary>
-    /// データ取得を行うインターフェイス
-    /// </summary>
     public interface IResolver<T>
     {
-        /// <summary>
-        /// データを取得する
-        /// </summary>
         T Resolve(Container container);
     }
 }

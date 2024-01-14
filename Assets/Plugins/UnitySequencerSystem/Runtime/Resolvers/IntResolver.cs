1172e1cfdd2d0f563270a78b20991ea8ab93ec7e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-14 22:00:19 +0900

    Refactor IntResolver subclasses

--- Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/IntResolver.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/IntResolver.cs
@@ -8,13 +8,22 @@ namespace UnitySequencerSystem.Resolvers
         public abstract int Resolve(Container container);
 
 #if USS_SUPPORT_SUB_CLASS_SELECTOR
-    [AddTypeMenu("Constant")]
+        [AddTypeMenu("Constant")]
 #endif
         [Serializable]
         public sealed class Constant : IntResolver
         {
             [SerializeField]
             private int value;
+
+            public Constant()
+            {
+            }
+
+            public Constant(int value)
+            {
+                this.value = value;
+            }
 
             public override int Resolve(Container container)
             {
@@ -23,13 +32,22 @@ namespace UnitySequencerSystem.Resolvers
         }
 
 #if USS_SUPPORT_SUB_CLASS_SELECTOR
-    [AddTypeMenu("Name")]
+        [AddTypeMenu("Name")]
 #endif
         [Serializable]
-        public sealed class NameFloat : IntResolver
+        public sealed class Name : IntResolver
         {
             [SerializeField]
             private string name;
+
+            public Name()
+            {
+            }
+
+            public Name(string name)
+            {
+                this.name = name;
+            }
 
             public override int Resolve(Container container)
             {

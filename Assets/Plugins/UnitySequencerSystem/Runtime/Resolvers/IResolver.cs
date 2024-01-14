5c089e8a30c14f4394f1e858f2bfbac372ce21ee
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-14 22:02:05 +0900

    Add XML documentation to IResolver interface

--- Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/IResolver.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/IResolver.cs
@@ -1,7 +1,16 @@
 namespace UnitySequencerSystem.Resolvers
 {
+    /// <summary>
+    /// Represents a resolver interface that resolves a type T using a container.
+    /// </summary>
+    /// <typeparam name="T">The type to be resolved.</typeparam>
     public interface IResolver<T>
     {
+        /// <summary>
+        /// Resolves the specified type T using the given container.
+        /// </summary>
+        /// <param name="container">The container used for resolving.</param>
+        /// <returns>The resolved instance of type T.</returns>
         T Resolve(Container container);
     }
 }

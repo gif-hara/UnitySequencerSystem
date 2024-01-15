d04267b21bf646ef53063675f42f7ad8ee51bd82
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-15 22:21:05 +0900

    Remove unused code for player movement

--- Assets/Plugins/UnitySequencerSystem/Samples/Sample01/Scripts/SceneController.cs
+++ Assets/Plugins/UnitySequencerSystem/Samples/Sample01/Scripts/SceneController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace UnitySequencerSystem.Samples.Sample01
@@ -9,30 +7,8 @@ namespace UnitySequencerSystem.Samples.Sample01
         [SerializeField]
         private Transform playerObject;
 
-        [SerializeField]
-        private float playerSpeed = 1;
-
         void Update()
         {
-            var velocity = Vector3.zero;
-            if (Input.GetKey(KeyCode.W))
-            {
-                velocity.y = 1;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                velocity.y = -1;
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                velocity.x = -1;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                velocity.x = 1;
-            }
-            velocity.Normalize();
-            playerObject.position += velocity * Time.deltaTime * playerSpeed;
         }
     }
 }

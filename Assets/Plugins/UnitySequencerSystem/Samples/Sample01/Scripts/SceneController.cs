fca3d53acdfb31f7225b8bc3f30a7cb561461a00
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-15 18:30:10 +0900

    Add player movement functionality

--- Assets/Plugins/UnitySequencerSystem/Samples/Sample01/Scripts/SceneController.cs
+++ Assets/Plugins/UnitySequencerSystem/Samples/Sample01/Scripts/SceneController.cs
@@ -6,5 +6,33 @@ namespace UnitySequencerSystem.Samples.Sample01
 {
     public class SceneController : MonoBehaviour
     {
+        [SerializeField]
+        private Transform playerObject;
+
+        [SerializeField]
+        private float playerSpeed = 1;
+
+        void Update()
+        {
+            var velocity = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                velocity.y = 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                velocity.y = -1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                velocity.x = -1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                velocity.x = 1;
+            }
+            velocity.Normalize();
+            playerObject.position += velocity * Time.deltaTime * playerSpeed;
+        }
     }
 }

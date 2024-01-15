a55abb7f4fad345e55d63aeb0ed1b6e58788ebf0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-15 22:31:32 +0900

    Add bullet spawning and behavior sequences

--- Assets/Plugins/UnitySequencerSystem/Samples/Sample01/Scripts/SceneController.cs
+++ Assets/Plugins/UnitySequencerSystem/Samples/Sample01/Scripts/SceneController.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnitySequencerSystem.Resolvers;
+using UnitySequencerSystem.StandardSequences;
 
 namespace UnitySequencerSystem.Samples.Sample01
 {
@@ -7,8 +11,27 @@ namespace UnitySequencerSystem.Samples.Sample01
         [SerializeField]
         private Transform playerObject;
 
+        [SerializeField]
+        private ScriptableSequences spawnBulletSequence;
+
+        [SerializeField]
+        private ScriptableSequences bulletBehaviourSequence;
+
         void Update()
         {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SpawnBulletAsync().Forget();
+            }
+        }
+
+        private async UniTask SpawnBulletAsync()
+        {
+            var container = new Container();
+            var spawnSequencer = new Sequencer(container, spawnBulletSequence.Sequences);
+            await spawnSequencer.PlayAsync(this.destroyCancellationToken);
+            var bulletBehaviourSequencer = new Sequencer(container, bulletBehaviourSequence.Sequences);
+            await bulletBehaviourSequencer.PlayAsync(this.destroyCancellationToken);
         }
     }
 }

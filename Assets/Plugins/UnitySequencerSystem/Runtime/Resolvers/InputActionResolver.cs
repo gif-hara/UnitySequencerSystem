0a0994c0be43a885dbcfb70a97b0d8047cdb3184
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-01-16 14:17:25 +0900

    Add ReferenceInputActionReference class to InputActionResolver

--- Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/InputActionResolver.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/Resolvers/InputActionResolver.cs
@@ -8,6 +8,17 @@ namespace UnitySequencerSystem.Resolvers
     public abstract class InputActionResolver : IResolver<InputAction>
     {
         public abstract InputAction Resolve(Container container);
+
+        public sealed class ReferenceInputActionReference : InputActionResolver
+        {
+            [SerializeField]
+            private InputActionReference inputActionReference;
+
+            public override InputAction Resolve(Container container)
+            {
+                return inputActionReference.action;
+            }
+        }
     }
 }
 #endif

7630680830a69598776b2db460aaaec26372efcd
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-12 15:12:58 +0900

    refactor: Add TransformShakeFloat classes for binding to transform shake motion

--- Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/External/LitMotion/LitMotion.BindTo.FloatShake.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/External/LitMotion/LitMotion.BindTo.FloatShake.cs
@@ -52,6 +52,114 @@ namespace UnitySequencerSystem.LitMotion
                 return motionBuilder.BindToEulerAnglesZ(targetResolver.Resolve(container));
             }
         }
+
+        [Serializable]
+        public sealed class TransformLocalEulerAnglesX : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToLocalEulerAnglesX(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformLocalEulerAnglesY : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToLocalEulerAnglesY(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformLocalEulerAnglesZ : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToLocalEulerAnglesZ(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformLocalPositionX : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToLocalPositionX(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformLocalPositionY : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToLocalPositionY(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformLocalPositionZ : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToLocalPositionZ(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformLocalScaleX : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToLocalScaleX(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformLocalScaleY : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToLocalScaleY(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformLocalScaleZ : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToLocalScaleZ(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformPositionX : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToPositionX(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformPositionY : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToPositionY(targetResolver.Resolve(container));
+            }
+        }
+
+        [Serializable]
+        public sealed class TransformPositionZ : TransformShakeFloat
+        {
+            public override MotionHandle BindTo(MotionBuilder<float, ShakeOptions, FloatShakeMotionAdapter> motionBuilder, Container container)
+            {
+                return motionBuilder.BindToPositionZ(targetResolver.Resolve(container));
+            }
+        }
     }
 }
 #endif

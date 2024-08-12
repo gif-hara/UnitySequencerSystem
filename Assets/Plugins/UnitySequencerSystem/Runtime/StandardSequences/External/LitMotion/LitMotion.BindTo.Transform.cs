698e43841c0e80bee6d2f8652470a4eafeb400f2
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-08-12 10:15:56 +0900

    refactor: Remove unused code and rename BindToTransform to BindToTransformVector3

--- Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/External/LitMotion/LitMotion.BindTo.Transform.cs
+++ Assets/Plugins/UnitySequencerSystem/Runtime/StandardSequences/External/LitMotion/LitMotion.BindTo.Transform.cs
@@ -21,6 +21,42 @@ namespace UnitySequencerSystem.LitMotion
     }
 
     [Serializable]
+    public sealed class BindToTransformEulerAngles : BindToTransformVector3
+    {
+        public override MotionHandle Bind(MotionBuilder<Vector3, NoOptions, Vector3MotionAdapter> motionBuilder, Container container)
+        {
+            return motionBuilder.BindToEulerAngles(targetResolver.Resolve(container));
+        }
+    }
+
+    [Serializable]
+    public sealed class BindToTransformLocalEulerAngles : BindToTransformVector3
+    {
+        public override MotionHandle Bind(MotionBuilder<Vector3, NoOptions, Vector3MotionAdapter> motionBuilder, Container container)
+        {
+            return motionBuilder.BindToLocalEulerAngles(targetResolver.Resolve(container));
+        }
+    }
+
+    [Serializable]
+    public sealed class BindToTransformLocalPosition : BindToTransformVector3
+    {
+        public override MotionHandle Bind(MotionBuilder<Vector3, NoOptions, Vector3MotionAdapter> motionBuilder, Container container)
+        {
+            return motionBuilder.BindToLocalPosition(targetResolver.Resolve(container));
+        }
+    }
+
+    [Serializable]
+    public sealed class BindToTransformLocalScale : BindToTransformVector3
+    {
+        public override MotionHandle Bind(MotionBuilder<Vector3, NoOptions, Vector3MotionAdapter> motionBuilder, Container container)
+        {
+            return motionBuilder.BindToLocalScale(targetResolver.Resolve(container));
+        }
+    }
+
+    [Serializable]
     public sealed class BindToTransformPosition : BindToTransformVector3
     {
         public override MotionHandle Bind(MotionBuilder<Vector3, NoOptions, Vector3MotionAdapter> motionBuilder, Container container)
